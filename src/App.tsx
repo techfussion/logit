f09084bcd4e0b09f1099fb0799b2e3fa186af9c0
techfussion <muminraj15@gmail.com>
2024-09-25 22:44:40 +0100

    sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
-import Login from './pages/Login'
+import Signup from './pages/Signup'
+// import Login from './pages/Login'
 
 function App() {
   return (
     <>
-      <Login />
+      <Signup />
     </>
   )
 }
